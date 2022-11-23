d44e45f4d1bc0890efae6b741041774a9b594ecc
rorschach17 <s0958952199@gmail.com>
2022-11-23 15:32:02 +0800

    test

--- pe/src/main/java/com/ctbc/pe/PeApplication.java
+++ pe/src/main/java/com/ctbc/pe/PeApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PeApplication {
-
-	
 	public static void main(String[] args) {
 		SpringApplication.run(PeApplication.class, args);
 	}
